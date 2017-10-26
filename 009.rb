b49249929a34328ae8d0b364735f2591d5448555
eloi <eloi@debian>
2017-10-26 18:04:59 +0200

    Add 9 to 11 solutions

--- 009.rb
+++ 009.rb
@@ -5,3 +5,15 @@
 # For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+(1..1000).each do |a|
+  (a + 1..1000).each do |b|
+    (b + 1.. 1000).each do |c|
+      if a + b + c == 1000
+        if a ** 2 + b ** 2 == c ** 2
+          puts "#{a} #{b} #{c}"
+        end
+      end
+    end
+  end
+end
