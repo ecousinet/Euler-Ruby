2653531832f01d4efd4087da31fec0a9dba6d088
eloi <eloi@debian>
2017-11-21 14:23:12 +0100

    Add result for 14 and init 15

--- 013.rb
+++ 013.rb
@@ -1,6 +1,6 @@
 # Problem 13
 #
-#Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
 CONST_STRING =
 '37107287533902102798797998220837590246510135740250'\
---
+++ 015.rb
@@ -0,0 +1,11 @@
+# Problem 15
+#
+# Starting in the top left corner of a 2×2 grid, and only being able to move to
+# the right and down, there are exactly 6 routes to the bottom right corner.
+#
+# How many such routes are there through a 20×20 grid?
+
+# Comment :
+# I initially thought about making a naive program that would explore all
+# the possibitliy iteratively but I felt that this problem could be strictly
+# answered by mathematics so I choose this option
